2c702773bfbc5eb06981fc81e92053dc6a285932
aryanpatel1918 <aryanpatel1918@gmail.com>
2025-11-19 16:39:55 -0500

    Completed BooksCard layout

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,18 +1,38 @@
 import { Link } from 'react-router-dom'
 import { PiBookOpenTextLight } from "react-icons/pi"
 import { BiUserCircle } from "react-icons/bi"
-import { AiOutlineEdit } from "react-icons/ai"
-import { BsInfoCircle } from "react-icons/bs"
-import { MdOutlineDelete } from "react-icons/md"
+import { BsInfoCircleFill } from "react-icons/bs"
+import { AiFillEdit } from "react-icons/ai"
+import { MdDelete } from "react-icons/md"
 
 export default function BooksCard({ books }) {
   return (
-    <>
+    <div className='grid gap-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
       {books.map((book, index) => (
-        <div>
-          <h3>{book.title}</h3>
+        <div key={index} className='border-2 border-gray-500 rounded-lg px-4 py-2 relative hover:shadow-xl'>
+          <h2 className='absolute top-1 right-1 px-3 py-1 bg-red-300 rounded-lg shadow'>{book.year}</h2>
+          <h4 className='my-2 text-gray-500'>{book._id}</h4>
+          <div className="flex justify-start items-center gap-x-2">
+            <PiBookOpenTextLight className='text-red-400 text-2xl' />
+            <h2 className="my-1">{book.title}</h2>
+          </div>
+          <div className='flex justify-start items-center gap-x-2'>
+            <BiUserCircle className='text-red-400 text-2xl' />
+            <h2 className='my-1'>{book.author}</h2>
+          </div>
+          <div className='flex justify-between items-center gap-x-2 mt-4 p-4'>
+            <Link to={`/books/details/${book._id}`} title="Book Details">
+              <BsInfoCircleFill className='text-2xl text-blue-500 hover:text-blue-600' />
+            </Link>
+            <Link to={`/books/edit/${book._id}`} title="Edit Book">
+              <AiFillEdit className='text-2xl text-yellow-500 hover:text-yellow-600' />
+            </Link>
+            <Link to={`/books/delete/${book._id}`} title="Delete Book">
+              <MdDelete className="text-2xl text-red-500 hover:text-red-600" />
+            </Link>
+          </div>
         </div>
       ))}
-    </>
+    </div>
   )
 }
--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
-import { AiOutlineEdit } from "react-icons/ai"
-import { BsInfoCircle } from "react-icons/bs"
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md"
+import { BsInfoCircleFill } from "react-icons/bs"
+import { AiFillEdit } from "react-icons/ai"
+import { MdDelete } from "react-icons/md"
 
 export default function BooksTable({ books }) {
   return (
@@ -43,13 +43,13 @@ export default function BooksTable({ books }) {
             <td className="border border-slate-700 rounded-md text-center">
               <div className="flex justify-center gap-x-4">
                 <Link to={`/books/details/${book._id}`} title="Book Details">
-                  <BsInfoCircle className="text-xl text-blue-500 hover:text-blue-600" />
+                  <BsInfoCircleFill className="text-xl text-blue-500 hover:text-blue-600" />
                 </Link>
                 <Link to={`/books/edit/${book._id}`} title="Edit Book">
-                  <AiOutlineEdit className="text-xl text-yellow-500 hover:text-yellow-600" />
+                  <AiFillEdit className="text-xl text-yellow-500 hover:text-yellow-600" />
                 </Link>
                 <Link to={`/books/delete/${book._id}`} title="Delete Book">
-                  <MdOutlineDelete className="text-xl text-red-600 hover:text-red-700" />
+                  <MdDelete className="text-xl text-red-600 hover:text-red-700" />
                 </Link>
               </div>
             </td>
