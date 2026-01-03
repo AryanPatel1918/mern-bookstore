3ab0ea1160ead99d7e05efce165790db52daafb5
Aryan Patel <97249070+AryanPatel1918@users.noreply.github.com>
2026-01-03 17:27:22 -0500

    Fix route path for creating a book

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='books/create' element={<CreateBook />} />
+      <Route path='/books/create' element={<CreateBook />} />
       <Route path='/books/details/:id' element={<ShowBook />} />
       <Route path='/books/edit/:id' element={<EditBook />} />
       <Route path='/books/delete/:id' element={<DeleteBook />} />
